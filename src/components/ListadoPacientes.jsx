1bc2715cbf17c557ff5bcc65a79e8243f0e2e101
dev-geampiere <jeanpiere.virgilio@gmail.com>
2022-07-17 15:09:59 -0500

    Mostrando textos de forma condicional

--- src/components/ListadoPacientes.jsx
+++ src/components/ListadoPacientes.jsx
@@ -4,21 +4,38 @@ const ListadoPacientes = ({pacientes}) => {
 
     return ( 
         <div className="md:w-1/2 lg:w-3/5 md:h-screen overflow-y-scroll">
-            <h2 className="font-black text-center text-xl">
-                Listado Pacientes
-            </h2>
-            <p className="text-l text-center mt-3 mb-1">
-                Administra tus {''}
-                <span className="text-indigo-600 font-bold">
-                    Pacientes y Citas
-                </span>
-            </p>
-            {pacientes.map( paciente =>(
-                <Pacientes 
-                    key = {paciente.id}
-                    paciente = {paciente}
-                />
-            ))}
+
+            { pacientes && pacientes.length ? (
+                <>
+                    <h2 className="font-black text-center text-xl">
+                        Listado Pacientes
+                    </h2>
+                    <p className="text-l text-center mt-3 mb-1">
+                        Administra tus {''}
+                        <span className="text-indigo-600 font-bold">
+                            Pacientes y Citas
+                        </span>
+                    </p>
+                    {pacientes.map( paciente =>(
+                        <Pacientes 
+                            key = {paciente.id} 
+                            paciente = {paciente}
+                        />
+                    ))}
+                </>
+            ) : (
+                <>
+                    <h2 className="font-black text-center text-xl">
+                        No hay pacientes
+                    </h2>
+                    <p className="text-l text-center mt-3 mb-1">
+                        Comienza agregando pacientes y {''}
+                        <span className="text-indigo-600 font-bold">
+                            apareceran en este lugar
+                        </span>
+                    </p>
+                </>
+            )}
         </div>
      );
 }
